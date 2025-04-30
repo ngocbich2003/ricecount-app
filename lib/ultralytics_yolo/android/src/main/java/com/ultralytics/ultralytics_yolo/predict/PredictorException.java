646688de2675123631fcb2be60d5577754a5c5a1
ngocbich2003 <daongocbich1305@gmail.com>
2025-04-30 19:17:52 +0700

    edit

--- lib/ultralytics_yolo/android/src/main/java/com/ultralytics/ultralytics_yolo/predict/PredictorException.java
+++
@@ -1,7 +0,0 @@
-package com.ultralytics.ultralytics_yolo.predict;
-
-public class PredictorException extends Exception{
-    public PredictorException(String message) {
-        super(message);
-    }
-}
